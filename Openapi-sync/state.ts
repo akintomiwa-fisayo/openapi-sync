145071c48f1fe33232f3dc78771add50219b37d0
akintomiwa-fisayo <akintomiwa.fisayo@gmail.com>
2024-08-13 22:28:49 +0100

    chore: use db.json to manage state

--- Openapi-sync/state.ts
+++ Openapi-sync/state.ts
@@ -1,9 +1,16 @@
+import path from "path";
 import { IOpenApiSpec } from "./types";
+import fs from "fs";
 
-let state: Record<string, IOpenApiSpec> = {};
+const dbPath = path.join(__dirname, "../", "../db.json");
+let state: Record<string, IOpenApiSpec> = require(dbPath);
 
+const updateDB = (data: typeof state) => {
+  fs.writeFileSync(dbPath, JSON.stringify(data));
+};
 export const setState = (key: string, value: IOpenApiSpec) => {
   state[key] = value;
+  updateDB(state);
 };
 
 export const getState = (key: string): IOpenApiSpec | undefined => {
@@ -12,4 +19,5 @@ export const getState = (key: string): IOpenApiSpec | undefined => {
 
 export const resetState = () => {
   state = {};
+  updateDB(state);
 };
