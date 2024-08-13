daf21ed6e6b81cf88cc72cf17cfab2187829de94
akintomiwa-fisayo <akintomiwa.fisayo@gmail.com>
2024-08-13 22:33:11 +0100

    feat: accept options when initialize openapi-sync

--- index.ts
+++ index.ts
@@ -7,18 +7,19 @@ dotenv.config();
 
 const rootUsingCwd = process.cwd();
 
-const Init = async () => {
+export const Init = async (options?: { refetchInterval?: number }) => {
   // Load config file
   const config = require(path.join(rootUsingCwd, "openapi.sync.json"));
   const apiNames = Object.keys(config.api);
-
+  const refetchInterval =
+    options?.refetchInterval && !isNaN(options.refetchInterval)
+      ? options.refetchInterval
+      : config.refetchInterval;
   resetState();
   for (let i = 0; i < apiNames.length; i += 1) {
     const apiName = apiNames[i];
     const apiUrl = config.api[apiName];
 
-    OpenapiSync(apiUrl, apiName);
+    OpenapiSync(apiUrl, apiName, refetchInterval);
   }
 };
-
-Init();
